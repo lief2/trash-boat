ca7f3ccd7b6b1efcb2eb3ddb4fc2ff5b0b6463d0
Moddingear <moddingear@gmail.com>
2019-11-23 17:20:08 +0100

    Gyro + FFT

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ T Clamp(T x, T minimum, T maximum)
 template<class T>
 T Lerp(T alpha, T a, T b)
 {
-  return a + T * (b-a);
+  return a + alpha * (b-a);
 }
 
 namespace Communication
@@ -99,6 +99,72 @@ namespace MesureEau{
   }
 }
 
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+#include "arduinoFFT.h"
+namespace MesureAngle{
+  #define SDA 21
+  #define SCL 22
+
+  MPU6050 mpu6050(Wire);
+
+  
+
+  /*
+  These values can be changed in order to evaluate the functions
+  */
+  const uint16_t samples = 256; //This value MUST ALWAYS be a power of 2
+  const double samplingFrequency = 250; //Hz
+  /*
+  These are the input and output vectors
+  Input vectors receive computed results from FFT
+  */
+  double vReal[samples];
+  double vImag[samples];
+  int currentIndex = 0;
+  long long unsigned int MeasureMicros = 0;
+  unsigned int MicrosPeriod = 1e6/samplingFrequency;
+
+  arduinoFFT FFT = arduinoFFT(vReal, vImag, samples, samplingFrequency);
+
+  void Task(void* Parameters)
+  {
+    Wire.begin(SDA, SCL, 400000L);
+    mpu6050.begin();
+    mpu6050.calcGyroOffsets(true, 0, 0);
+    for (;;)
+    {
+      delayMicroseconds(MeasureMicros + MicrosPeriod - micros());
+      mpu6050.update();
+      MeasureMicros += MicrosPeriod;
+      float AngleDeg = mpu6050.getAngleX();
+      if(currentIndex < samples)
+      {
+        vReal[currentIndex] = AngleDeg;
+        vImag[currentIndex] = 0.0;
+        currentIndex++;
+      }
+      else
+      {
+        for (int i = 1; i < samples; i++)
+        {
+          vReal[i-1] = vReal[i];
+          vImag[i-1] = 0.0;
+        }
+        vReal[currentIndex] = AngleDeg;
+        vImag[currentIndex] = 0.0;
+      }
+      FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+      FFT.Compute(FFT_FORWARD);
+      double RollFrequency, RollAmplitude; 
+      FFT.MajorPeak(&RollFrequency, &RollAmplitude);
+      //vTaskDelay(1);
+    }
+    
+    vTaskDelete(NULL);
+  }
+}
+
 namespace Asservissement{
   const float Kp = 1, Ki = 1, Kd = 1;
   const float AbsIMax = 1;
@@ -174,7 +240,7 @@ namespace Stepper{
       stepper.run();
       vTaskDelay(1);
     }
-    
+
     vTaskDelete(NULL);
   }
 }
@@ -182,7 +248,8 @@ void setup() {
   Serial.begin(115200);
   Communication::InitSemaphores();
   xTaskCreatePinnedToCore(MesureEau::Task, "Mesures du niveau d'eau", 10000, NULL, 1, NULL, 0);
-  xTaskCreatePinnedToCore(Asservissement::Task, "Asservissement", 10000, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(MesureAngle::Task, "Mesure de l'angle", 10000, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(Asservissement::Task, "Asservissement", 10000, NULL, 1, NULL, 0);
   xTaskCreatePinnedToCore(Stepper::Task, "Moteur", 10000, NULL, 1, NULL, 1);
   vTaskDelete(NULL);
 }
